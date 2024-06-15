2e15381242810859ede69761f1622230407fa1ee
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2024-06-15 15:33:11 +0600

    Updated success, failed and canceled page

--- src/components/Canceled/Canceled.jsx
+++ src/components/Canceled/Canceled.jsx
@@ -1,7 +1,7 @@
 const Canceled = () => {
   return (
     <div className="card card-compact container rounded-xl mx-auto max-w-fit">
-      <h2 className="font-bold text-6xl">Payment Canceled</h2>
+      <h2 className="font-bold text-6xl">Investment Canceled</h2>
     </div>
   );
 };
--- src/components/Success/Success.jsx
+++ src/components/Success/Success.jsx
@@ -1,9 +1,7 @@
 const Success = () => {
   return (
     <div className="card card-compact container rounded-xl mx-auto max-w-fit">
-      <h2 className="font-bold text-6xl">
-        Payment Successful
-      </h2>
+      <h2 className="font-bold text-6xl">Successfully Invested</h2>
     </div>
   );
 };
