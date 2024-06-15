13b92f33c6a2a49ff71b1a4c851fc3b8f9254f72
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2024-06-15 15:21:17 +0600

    Added payment success page

---
+++ src/components/Success/Success.jsx
@@ -0,0 +1,9 @@
+const Success = () => {
+  return (
+    <div className="card card-compact container rounded-xl mx-auto max-w-fit">
+      <h2 className="font-bold text-6xl">Payment Successful</h2>
+    </div>
+  );
+};
+
+export default Success;
