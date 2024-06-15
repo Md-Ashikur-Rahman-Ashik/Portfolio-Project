21874236df880e2217c45b349dd666596b2f6001
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2024-06-15 15:22:35 +0600

    Added payment failed page

---
+++ src/components/Failed/Failed.jsx
@@ -0,0 +1,9 @@
+const Failed = () => {
+  return (
+    <div className="card card-compact container rounded-xl mx-auto max-w-fit">
+      <h2 className="font-bold text-6xl">Payment Failed</h2>
+    </div>
+  );
+};
+
+export default Failed;
