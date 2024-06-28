480e4f7f13644528b44133be20b3ab26f4f9b0ba
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2024-06-28 18:06:04 +0600

    Adding about section

---
+++ src/components/About/About.jsx
@@ -0,0 +1,10 @@
+const About = () => {
+  return (
+    <div className="container mx-auto mt-20 flex justify-center gap-4">
+      <div>Content</div>
+      <div>Image</div>
+    </div>
+  );
+};
+
+export default About;
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,5 +1,6 @@
 import { Helmet } from "react-helmet-async";
 import Banner from "../Banner/Banner";
+import About from "../About/About";
 
 const Home = () => {
   return (
@@ -8,6 +9,7 @@ const Home = () => {
         <title>IFRAN ISLAM</title>
       </Helmet>
       <Banner></Banner>
+      <About></About>
     </div>
   );
 };
